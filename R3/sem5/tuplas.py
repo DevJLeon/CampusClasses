e8b3e56b7c63455b4693a7ba1b0201f5efb4b829
Joshua <celisjoshleon@gmail.com>
2023-04-17 15:12:49 -0500

    sorting dictionaries

--- R3/sem5/tuplas.py
+++ R3/sem5/tuplas.py
@@ -1,16 +1,14 @@
-miLista = ["Juan", "Maria", "José", "Tulio", "Armando"]
+miLista = [{"nombre": "Patricio","edad":21},{"nombre":"Juan", "edad":18},{"nombre":"Juan", "edad":38},{"nombre":"Juan", "edad":8}]
 
 miTupla = (1,2,3)
-
-miLista.sort(reverse=True)
 
 m = len(miLista)
 
 for i in range(m-1):
     for j in range (i+1,m):
-        if miLista[i]>miLista[j]:
-            t = miLista[i]
-            miLista[i] = miLista[j]
-            miLista[j]=t
+        if miLista[i]["edad"]>miLista[j]["edad"]:
+            t = miLista[i]["edad"]
+            miLista[i]["edad"] = miLista[j]["edad"]
+            miLista[j]["edad"]=t
         
 print(miLista)
