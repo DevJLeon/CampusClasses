77277eb2e102c2c0a8602cc9dff9a65065a803de
Joshua <celisjoshleon@gmail.com>
2023-04-17 15:26:11 -0500

    Tarea xd

---
+++ R3/sem5/exEstructuraDatos.py
@@ -0,0 +1,1 @@
+#se desea realizar un programa en el cual se ingresen N letras edl abecedario las cuales se deben almacenar en una lista. Una vez creada la lista, se desea conocer e imprimir la cantidad de "a", la cantidad de e , la cantidad de i, o y u que se encuentran en la lista.
