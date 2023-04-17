c947ffda4d79b8b9c10df9aa6223b15c99b3c494
Joshua <celisjoshleon@gmail.com>
2023-04-17 14:53:52 -0500

    prueba

--- R3/sem5/tuplas.py
+++ R3/sem5/tuplas.py
@@ -1,5 +1,16 @@
-miLista = []
+miLista = ["Juan", "Maria", "José", "Tulio", "Armando"]
 
 miTupla = (1,2,3)
 
-miLista.sort()
+miLista.sort(reverse=True)
+
+m = len(miLista)
+
+for i in range(m-1):
+    for j in range (i+1,m):
+        if miLista[i]>miLista[j]:
+            t = miLista[i]
+            miLista[i] = miLista[j]
+            miLista[j]=t
+        
+print(miLista)
