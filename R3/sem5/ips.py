0501ccf28952ff2038a46a24ddb33f74319fac9b
Joshua <celisjoshleon@gmail.com>
2023-04-18 16:42:21 -0500

    edad

--- R3/sem5/ips.py
+++ R3/sem5/ips.py
@@ -1,28 +1,24 @@
-#Datos basicos:
-#codigo
-#nombre
-#edad
-#peso
-#sexo
-
+from datetime import date
 
 def addPacient():
     codigo = len(pacientes)+1
     name = input("Ingrese el nombre del paciente: \t")
     bDay = input("Ingrese la fecha de nacimiento del paciente: (dd/mm/aaaa) \t")
+    age = bDay.split("/")
+    age = calculateAge(date(int(age[2]),int(age[1]),int(age[0])))
     weight = float(input("Ingrese el peso del paciente en kg: \t"))
     gender = int(input("Indique el sexo del paciente \n 0. Masculino 1.Femenino: \t"))
     if gender == 0:
         gender = "Masculino"
     elif gender == 1:
         gender = "Femenino"
-    pacientes.append({"codigo":codigo,"nombre":name,"fechaNacimiento":bDay,"peso":weight,"sexo":gender})
+    pacientes.append({"codigo":codigo,"nombre":name,"fechaNacimiento":bDay,"edad":age,"peso":weight,"sexo":gender})
     return(pacientes)
 
 def showPacients():
     list = "CODIGO \t NOMBRE \t FECHA NACIMIENTO \t EDAD \t PESO \t SEXO \n"
     for paciente in range(len(pacientes)):
-        list += (str(pacientes[paciente]["codigo"])+ "   \t"+str(pacientes[paciente]["nombre"])+ "   \t"+str(pacientes[paciente]["fechaNacimiento"])+ "   \t"+str(pacientes[paciente]["peso"])+ "   \t"+str(pacientes[paciente]["sexo"])+ "\n")
+        list += (str(pacientes[paciente]["codigo"])+ "   \t"+str(pacientes[paciente]["nombre"])+ "   \t"+str(pacientes[paciente]["fechaNacimiento"])+"   \t \t "+str(pacientes[paciente]["edad"])+ "   \t"+str(pacientes[paciente]["peso"])+ "   \t"+str(pacientes[paciente]["sexo"])+ "\n")
     print(list)
     
 def editPacient():
@@ -75,6 +71,10 @@ def deleteInfo():
         cambios = int(input("¿Qué desea cambiar? \n 1. Nombre \t 2. Fecha Nacimiento \n 3. Peso \t 4. sexo \n 0. Nada(volver)"))
     return(pacientes)
         
+def calculateAge(birthDate):
+    days_in_year = 365.2425   
+    age = int((date.today() - birthDate).days / days_in_year)
+    return age
 
 pacientes = []
 
@@ -86,7 +86,7 @@ while opcion != 0:
     elif opcion == 2:
         editPacient()
     elif opcion == 3:
-        None
+        deleteInfo()
     elif opcion == 4:
             showPacients()
 
