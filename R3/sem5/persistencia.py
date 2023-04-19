f7fcdbf6113584fe990378f6facfefc9fba9a45a
Joshua <celisjoshleon@gmail.com>
2023-04-19 15:22:29 -0500

    reading lines

--- R3/sem5/persistencia.py
+++ R3/sem5/persistencia.py
@@ -1,9 +1,13 @@
 #1. CREAR 2. APERTURA 3. MANIPULACION 4. CIERRE
 #   OPEN     WRITE       READ /READLINES  CLOSE
 import io
-archivo = open("R3/sem5/nombres.txt","w")
+archivo = open("R3/sem5/nombres.txt","r")
 
-nom = "Sergio Medina \nLuisa Ruiz \nMario Moreno"
-archivo.write(nom)
-
+#nom = "Sergio Medina \nLuisa Ruiz \nMario Moreno"
+#archivo.write(nom)
+lista = archivo.readlines()
 archivo.close()
+print(lista)
+
+for i in range(len(lista)):
+    print("nombre:", lista[i])
