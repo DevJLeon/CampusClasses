e6822ef1f9484fcfca2df7df5c6ce63e9896cfe5
Joshua <celisjoshleon@gmail.com>
2023-04-21 17:36:43 -0500

    CRUD Finished

--- R3/sem5/SIMULACRO/simulacro.py
+++ R3/sem5/SIMULACRO/simulacro.py
@@ -6,9 +6,11 @@ with open("R3/sem5/SIMULACRO/Biblioteca.json") as biblioteca:
     bookstore = access_b["bookstore"]
     books = bookstore["book"]
 
+
 def save():
     with open("R3/sem5/SIMULACRO/Biblioteca.json", "w") as save:
-        json.dump(access_b,save, indent=4)
+        json.dump(access_b, save, indent=4)
+
 
 def addBook():
     lang = input("language of the book: \t")
@@ -20,7 +22,7 @@ def addBook():
         if authorsAmmount == 1:
             authors = input("Name of author: \t")
         else:
-            print("Name of ("+ str(i+1)+ ") author: \t")
+            print("Name of (" + str(i+1) + ") author: \t")
             author = input()
             authorList.append(author)
             authors = authorList
@@ -40,7 +42,7 @@ def addBook():
         "_category": category
     }
     books.append(newbook)
-    
+
     save()
 
 
@@ -52,24 +54,83 @@ def readBooks():
         for libro in books:
             time.sleep(1)
             print("\n------------------------------------------------------------\nTitle:",
-                libro["title"]["__text"]+"\nAuthor/s:"
-                )
+                  libro["title"]["__text"]+"\nAuthor/s:"
+                  )
             if type(libro["author"]) == list:
                 for autor in libro["author"]:
-                    print("*",autor)
+                    print("*", autor)
             else:
-                print("*",libro["author"])
-            print("Publish year:", libro["year"]+"\nPrice:", libro["price"],"$","USD","\nCategory:", libro["_category"])
+                print("*", libro["author"])
+            print("Publish year:", libro["year"]+"\nPrice:",
+                  libro["price"], "$", "USD", "\nCategory:", libro["_category"])
+
+
+def updateBook():
+    print("Choose wich book you want to update:")
+    for i in range(len(books)):
+        print(str(i)+": ", books[i]["title"]["__text"])
+    nombre = input("Name of the book to update: \t")
+    updateID = books.index(nombre)
+
+    libro = books[updateID]
+    print("\n------------------------------------------------------------\nTitle:",
+          libro["title"]["__text"]+"\nAuthor/s:")
+    if type(libro["author"]) == list:
+        for autor in libro["author"]:
+            print("*", autor)
+    else:
+        print("*", libro["author"])
+    print("Publish year:", libro["year"]+"\nPrice:",
+          libro["price"], "$", "USD", "\nCategory:", libro["_category"])
+
+    change = int(input(
+        "What do you want to update?: \n1.Title 2.Authors\n3.Publish year 4.Price\n5.Category 0.EXIT\n"))
+
+    while change > 0:
+        if change == 1:
+            libro["title"]["__text"] = input("Write the new name: \t")
+        elif change == 2:
+            authorsAmmount = int(
+                input("How many authors does this book have?: \t"))
+            authorList = []
+            for i in range(authorsAmmount):
+                if authorsAmmount == 1:
+                    authors = input("Name of author: \t")
+                else:
+                    print("Name of (" + str(i+1) + ") author: \t")
+                    author = input()
+                    authorList.append(author)
+                    authors = authorList
+            libro["author"] = authors
+        elif change == 3:
+            libro["year"] = input("Write the new publish year: \t")
+        elif change == 4:
+            libro["price"] = input("Write the new price: \t")
+        elif change == 5:
+            libro["_category"] = input("Write the new category: \t")
+        change = int(input(
+            "What do you want to update?: \n1.Title 2.Authors\n3.Publish year 4.Price\n5.Category 0.EXIT\n"))
 
 def deleteBook():
     print("Choose wich book you want to delete:")
     for i in range(len(books)):
-        print(str(i)+": ",books[i]["title"]["__text"])
+        print(str(i)+": ", books[i]["title"]["__text"])
     deleteID = int(input("Write the ID to delete: "))
     books.pop(deleteID)
     save()
-    
 
-deleteBook()
-#addBook()        
-#readBooks()
+
+menuopt = int(input("Select an option: \n1.Add a new book 2.Search Books \n3.Update an existing book 4.Delete a book\n 0.EXIT: \t"))
+
+while menuopt >0:
+    if menuopt == 1:
+        addBook()
+    elif menuopt == 2:
+        readBooks()
+    elif menuopt == 3:
+        updateBook()
+    elif menuopt == 4:
+        deleteBook()
+    menuopt = int(input("Select an option: \n1.Add a new book 2.Search Books \n3.Update an existing book 4.Delete a book\n 0.EXIT: \t"))
+
+
